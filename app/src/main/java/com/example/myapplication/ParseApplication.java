921cfb5156c61f2cc76f38a6948c23c37de66deb
Adnan <adnank9465@gmail.com>
2021-10-17 23:16:51 -0700

    Added some changes + drawable Instagram resource files

--- app/src/main/java/com/example/myapplication/ParseApplication.java
+++ app/src/main/java/com/example/myapplication/ParseApplication.java
@@ -2,12 +2,17 @@ package com.example.myapplication;
 
 import android.app.Application;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
                 .clientKey(getString(R.string.back4app_client_key))
